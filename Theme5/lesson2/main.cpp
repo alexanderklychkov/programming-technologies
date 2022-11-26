e3d3723425112e577e7fd3a24dafae88836bb835
Alexander Klychkov <caany408@gmail.com>
2022-11-26 23:33:03 +0300

    Fix lesson 5

--- Theme5/lesson2/main.cpp
+++ Theme5/lesson2/main.cpp
@@ -8,8 +8,18 @@ main()
     printf("Count rows: ");
     scanf("%d", &rows);
 
+    if (rows > 100) {
+        printf("Rows cannot be more than 100");
+        return 0;
+    }
+
     printf("Count columns: ");
     scanf("%d", &columns);
+
+    if (columns > 100) {
+        printf("Columns cannot be more than 100");
+        return 0;
+    }
 
     for (int j = 0; j < rows; j++)
     {
