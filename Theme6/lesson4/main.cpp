68320f9907b13b59418919c16ff28abb799a5c7c
Alexander Klychkov <caany408@gmail.com>
2022-12-06 21:38:58 +0300

    Add HW 6.4

---
+++ Theme6/lesson4/main.cpp
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <string.h>
+
+main()
+{
+    int taskNumber;
+    char str[100];
+
+    printf("Write string: ");
+    gets(str);
+
+    // 1. Создать одномерный массив символов, вписать с клавиатуры данные, затем прописать код так, чтобы программа удаляла первую букву.
+    // 2. Создать зеркальное отображение строки с использованием второй (буферной) строки.
+    printf("Select task(1, 2): ");
+    scanf("%i", &taskNumber);
+
+    switch (taskNumber)
+    {
+        case 1:
+            for (int i = 0; i < strlen(str); i++)
+            {
+                str[i] = str[i + 1];
+            }
+
+            puts(str);
+            break;
+        case 2:
+            {
+                char replacedStr[100];
+                int i = 0;
+
+                for (int j = strlen(str) - 1; j >= 0; j--)
+                {
+                    replacedStr[i] = str[j];
+                    i++;    
+                }
+
+                replacedStr[i] = '\0';
+
+                puts(replacedStr);
+            }
+            break;
+        default:
+            printf("Task not found!");
+            break;
+    }
+}
